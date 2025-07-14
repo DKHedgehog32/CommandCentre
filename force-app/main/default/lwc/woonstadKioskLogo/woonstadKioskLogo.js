ad775b9a21aad41d4f0fef6b9fa95b40a7aced3d
dennismusschenbroek-kristoffers <default@cloudity.com>
2025-07-14 12:19:29 +0200

    Initial commit of DevB metadata for Kiosk setup

---
+++ force-app/main/default/lwc/woonstadKioskLogo/woonstadKioskLogo.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import LOGO_URL from '@salesforce/resourceUrl/WSRLogo';
+
+export default class WoonstadKioskLogo extends LightningElement {
+    logoUrl = LOGO_URL;
+}
